f49bf58687c731550d01825a4df18e404f163a62
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-04-19 08:07:16 +0100

    Update UserDetail.js

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -42,23 +42,28 @@ const UserDetail = () => {
     }
     return (
         <>
-            <div className="user-details">
+            <div className="container">
                 { IsPending && <div>Loading...</div>}
                 { error && <div>{ error }</div> }
-                { user && user.name ? (<div className="card">
-                <div className="card-title">{ user.name }</div>
-                <div className="card-body">
-                    <p>Username: { user.username } </p>
-                    <p>Email: { user.email } </p>
-                    <p>Phone: { user.phone } </p>
-                    
-                    <button className="btn btn-danger btn-sm me-1" onClick={handleDelete}><i className="bi bi-trash"></i></button>
-                    <button className="btn btn-primary btn-sm"><i className="bi bi-pen"></i></button>
+                { user && user.name ? (
+                    <div className="row mt-3 justify-content-center">
+                        <div className="col-4">
+                            <div className="card">
+                                {/* <img src="img.jpg" alt={ user.name } style={{width:"100%"}} /> */}
+                                <h1>{ user.name }</h1>
+                                <div className="card-title">{ user.username }</div>
+                                <div className="card-body">
+                                    <p>{ user.email } </p>
+                                    <p>{ user.phone } </p>
+                                    
+                                    <button className="btn btn-danger btn-sm me-1" onClick={handleDelete}><i className="bi bi-trash"></i></button>
+                                    <button className="btn btn-primary btn-sm"><i className="bi bi-pen"></i></button>
+                                </div>
+                        
+                            </div>
+                        </div>
+                    </div>
 
-                
-                </div>
-                
-                </div>
                 ): 'User not found'}
             </div>
         </>
