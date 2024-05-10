372902b7129f92ee62ea3e87f0a9aa1e59616d5f
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-05-10 14:56:57 +0100

    Update UserDetail.js

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -1,4 +1,4 @@
-import { useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import useFetch from "./useFetch";
